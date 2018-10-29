5160046c02024518587349cf28ac3a3a058556c7
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-10-29 15:49:26 +0700

    Add RunAbleThread.cs

---
+++ UnityProject/Assets/Scripts/RunAbleThread.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+
+/// <summary>
+/// The superclass that you should derive from. It provides Start() and Stop() method and Running property.
+/// It will start the thread to run Run() when you call Start().
+/// </summary>
+public abstract class RunAbleThread
+{
+    private readonly Thread _requestThread;
+    protected bool Running { get; private set; }
+
+    protected RunAbleThread()
+    {
+        // we need to create a thread instead of calling Run() directly because it would block unity
+        // from doing other tasks like drawing game scenes
+        _requestThread = new Thread(Run);
+    }
+
+    protected abstract void Run();
+
+    public void Start()
+    {
+        Running = true;
+        _requestThread.Start();
+    }
+
+    public void Stop()
+    {
+        Running = false;
+        // block main thread, wait for _requestThread to finish its job first, so we can be sure that 
+        // _requestThread will end before main thread end
+        _requestThread.Join();
+    }
+}
