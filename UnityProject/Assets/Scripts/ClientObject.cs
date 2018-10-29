1013d9497e546165fab2cca958f2377d95931066
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-10-29 14:09:46 +0700

    Remove unused code comments

--- UnityProject/Assets/Scripts/ClientObject.cs
+++ UnityProject/Assets/Scripts/ClientObject.cs
@@ -18,7 +18,7 @@ public class NetMqListener
 
     private void ListenerWork()
     {
-        AsyncIO.ForceDotNet.Force();
+        AsyncIO.ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
         using (var client = new RequestSocket())
         {
             client.Connect("tcp://localhost:5555");
@@ -32,21 +32,8 @@ public class NetMqListener
                 Debug.Log($"Received {message}");
             }
         }
-//        using (var subSocket = new SubscriberSocket())
-//        {
-//            subSocket.Options.ReceiveHighWatermark = 1000;
-//            subSocket.Connect("tcp://localhost:12345");
-//            subSocket.Subscribe("");
-//            while (!_listenerCancelled)
-//            {
-//                string frameString;
-//                if (!subSocket.TryReceiveFrameString(out frameString)) continue;
-//                Debug.Log(frameString);
-//                _messageQueue.Enqueue(frameString);
-//            }
-//            subSocket.Close();
-//        }
-        NetMQConfig.Cleanup();
+
+        NetMQConfig.Cleanup(); // this line is needed to prevent unity freeze after one use, not sure why yet
     }
 
     public void Update()
@@ -115,107 +102,3 @@ public class ClientObject : MonoBehaviour
         _netMqListener.Stop();
     }
 }
-//using System.Collections.Concurrent;
-//using System.Threading;
-//using NetMQ;
-//using UnityEngine;
-//using NetMQ.Sockets;
-//
-//public class NetMqListener
-//{
-//    private readonly Thread _listenerWorker;
-//
-//    private bool _listenerCancelled;
-//
-//    public delegate void MessageDelegate(string message);
-//
-//    private readonly MessageDelegate _messageDelegate;
-//
-//    private readonly ConcurrentQueue<string> _messageQueue = new ConcurrentQueue<string>();
-//
-//    private void ListenerWork()
-//    {
-//        AsyncIO.ForceDotNet.Force();
-//        using (var subSocket = new SubscriberSocket())
-//        {
-//            subSocket.Options.ReceiveHighWatermark = 1000;
-//            subSocket.Connect("tcp://localhost:12345");
-//            subSocket.Subscribe("");
-//            while (!_listenerCancelled)
-//            {
-//                string frameString;
-//                if (!subSocket.TryReceiveFrameString(out frameString)) continue;
-//                Debug.Log(frameString);
-//                _messageQueue.Enqueue(frameString);
-//            }
-//            subSocket.Close();
-//        }
-//        NetMQConfig.Cleanup();
-//    }
-//
-//    public void Update()
-//    {
-//        while (!_messageQueue.IsEmpty)
-//        {
-//            string message;
-//            if (_messageQueue.TryDequeue(out message))
-//            {
-//                _messageDelegate(message);
-//            }
-//            else
-//            {
-//                break;
-//            }
-//        }
-//    }
-//
-//    public NetMqListener(MessageDelegate messageDelegate)
-//    {
-//        _messageDelegate = messageDelegate;
-//        _listenerWorker = new Thread(ListenerWork);
-//    }
-//
-//    public void Start()
-//    {
-//        _listenerCancelled = false;
-//        _listenerWorker.Start();
-//    }
-//
-//    public void Stop()
-//    {
-//        _listenerCancelled = true;
-//        _listenerWorker.Join();
-//    }
-//}
-//
-//public class ClientObject : MonoBehaviour
-//{
-//    private NetMqListener _netMqListener;
-//
-//    private void HandleMessage(string message)
-//    {
-//        Debug.Log("Received Message: " + message);
-//        var splittedStrings = message.Split(' ');
-//        if (splittedStrings.Length != 3) return;
-//        var x = float.Parse(splittedStrings[0]);
-//        var y = float.Parse(splittedStrings[1]);
-//        var z = float.Parse(splittedStrings[2]);
-//        transform.position = new Vector3(x, y, z);
-//    }
-//
-//    private void Start()
-//    {
-//        _netMqListener = new NetMqListener(HandleMessage);
-//        _netMqListener.Start();
-//    }
-//
-//    private void Update()
-//    {
-//        _netMqListener.Update();
-//    }
-//
-//    private void OnDestroy()
-//    {
-//        _netMqListener.Stop();
-//    }
-//}
