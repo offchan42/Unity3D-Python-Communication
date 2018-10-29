e01285b6d10c1f31185cf7e06edd434af992d24c
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-10-29 15:57:50 +0700

    Rename _requestThread to _runnerThread

--- UnityProject/Assets/Scripts/RunAbleThread.cs
+++ UnityProject/Assets/Scripts/RunAbleThread.cs
@@ -1,34 +1,40 @@
 ﻿using System.Threading;
 
 /// <summary>
-/// The superclass that you should derive from. It provides Start() and Stop() method and Running property.
-/// It will start the thread to run Run() when you call Start().
+///     The superclass that you should derive from. It provides Start() and Stop() method and Running property.
+///     It will start the thread to run Run() when you call Start().
 /// </summary>
 public abstract class RunAbleThread
 {
-    private readonly Thread _requestThread;
-    protected bool Running { get; private set; }
+    private readonly Thread _runnerThread;
 
     protected RunAbleThread()
     {
         // we need to create a thread instead of calling Run() directly because it would block unity
         // from doing other tasks like drawing game scenes
-        _requestThread = new Thread(Run);
+        _runnerThread = new Thread(Run);
     }
 
+    protected bool Running { get; private set; }
+
+    /// <summary>
+    /// This method will get called when you call Start(). Programmer must implement this method while making sure that
+    /// this method terminates in a finite time. You can use Running property (which will be set to false when Stop() is
+    /// called) to determine when you should stop the method.
+    /// </summary>
     protected abstract void Run();
 
     public void Start()
     {
         Running = true;
-        _requestThread.Start();
+        _runnerThread.Start();
     }
 
     public void Stop()
     {
         Running = false;
-        // block main thread, wait for _requestThread to finish its job first, so we can be sure that 
-        // _requestThread will end before main thread end
-        _requestThread.Join();
+        // block main thread, wait for _runnerThread to finish its job first, so we can be sure that 
+        // _runnerThread will end before main thread end
+        _runnerThread.Join();
     }
 }
